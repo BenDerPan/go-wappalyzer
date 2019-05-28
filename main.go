b346dd2f3aacaa7efdc93fae942510de89caab99
benderpan <272569492@qq.com>
2019-05-28 19:17:11 +0800

    v1.0版本完成

--- main.go
+++ main.go
@@ -2,25 +2,151 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"github.com/benderpan/go-wappalyzer/wappalyzer"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
 	"os"
+	"runtime"
+	"strings"
 )
 
+var (
+	runType    string
+	update     bool
+	outputFile string
+	workers    int
+	apps       string
+	host       string
+	hosts      string
+	crawlCount int
+	listen     string
+	verbose    bool
+)
+
+func init() {
+	flag.StringVar(&runType,"type","standalone","run type, standalone - single tool model, web - web server model")
+	flag.StringVar(&outputFile, "output", "data.json", "output file")
+	flag.BoolVar(&update, "update", false, "update apps file")
+	flag.IntVar(&workers, "worker", 4, "number of concurrent worker")
+	flag.StringVar(&apps, "apps", "apps.json", "app definition file.")
+	flag.StringVar(&host, "host", "", "single host to test")
+	flag.StringVar(&hosts, "hosts", "", "filename with hosts, one host per line.")
+	flag.IntVar(&crawlCount, "crawl", 0, "links to follow from the root page (default 0)")
+	flag.StringVar(&listen,"listen","0.0.0.0:8080","Web server listen address")
+	flag.BoolVar(&verbose,"verbose",true,"show verbose message")
+
+
+	if cpu := runtime.NumCPU(); cpu == 1 {
+		runtime.GOMAXPROCS(2)
+	} else {
+		runtime.GOMAXPROCS(cpu)
+	}
+}
+
 func main() {
-	was,err:=wappalyzer.NewWebAnalyzeServer()
-	if err!=nil{
-		fmt.Printf("Error:%v",err)
-		return
+
+	flag.Parse()
+	if runType =="web" {
+		//run as web server, which will provide restfull api.
+		was, err := wappalyzer.NewWebAnalyzeServer(apps)
+		if err != nil {
+			fmt.Printf("Error:%v", err)
+			return
+		}
+
+		http.Handle("/", http.FileServer(FS(false)))
+		//http.Handle("/",http.StripPrefix("/", http.FileServer(http.Dir("./www/"))))
+		http.HandleFunc("/analyze",was.HttpHandlerAnalyze)
+		err=http.ListenAndServe(listen,nil)
+		if err!=nil {
+			log.Fatal("open web server failed, err: ", err)
+		}
+
+	}else{
+		var file io.ReadCloser
+		var err error
+
+		if !update && host == "" && hosts == "" {
+			flag.Usage()
+			return
+		}
+
+		if update {
+			err = wappalyzer.DownloadFile(wappalyzer.WappalyzerURL, "apps.json")
+			if err != nil {
+				log.Fatalf("error: can not update apps file: %v", err)
+			}
+
+			log.Println("app definition file updated from ", wappalyzer.WappalyzerURL)
+
+			if host == "" && hosts == "" {
+				return
+			}
+
+		}
+
+		// check single host or hosts file
+		if host != "" {
+			file = ioutil.NopCloser(strings.NewReader(host))
+		} else {
+			file, err = os.Open(hosts)
+			if err != nil {
+				log.Fatalf("error: can not open host file %s: %s", hosts, err)
+			}
+		}
+		defer file.Close()
+
+		results, err := wappalyzer.Init(workers, file, apps, crawlCount)
+
+		if err != nil {
+			log.Fatal("error initializing: ", err)
+		}
+
+		log.Printf("Scanning with %v workers.", workers)
+
+		outFile, err := os.OpenFile(outputFile, os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			log.Fatal("output file create failed. err: ", err)
+		}
+		defer outFile.Close()
+
+		for result := range results {
+			if result.Error != "" {
+				if verbose{
+					log.Printf("Error: Host=%s  Msg=%s", result.Host, result.Error)
+				}
+
+			}
+
+			jsonValue, err := json.Marshal(result)
+			if err != nil {
+				if verbose{
+					log.Print("{}\n")
+				}
+			} else {
+				jsonValue = append(jsonValue, '\n')
+				_,err= outFile.Write(jsonValue)
+				if err!=nil{
+					log.Fatal("write message to output file failed, err: ",err)
+				}
+				if verbose{
+					log.Print(string(jsonValue))
+				}
+
+
+			}
+
+		}
+
 	}
 
-	result:=was.Analyze("https://github.com")
-	jsonValue, err := json.Marshal(result)
-	if err != nil {
-		os.Stdout.Write([]byte("{}\n"))
-	} else {
-		jsonValue = append(jsonValue, '\n')
-		os.Stdout.Write(jsonValue)
-	}
+
+
+
+
 
 }
--- wappalyzer/webanalyze_http.go
+++ wappalyzer/webanalyze_http.go
@@ -1,7 +1,9 @@
 package wappalyzer
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
 	"net/url"
 	"time"
 )
@@ -10,18 +12,23 @@ type WebAnalyzeHttpServer struct {
 
 }
 
+type ResponseMsg struct {
+	Msg string `json:"msg"`
+	Code int `json:"code"`
+	Data string `json:"data"`
+}
+
 var webAnalyzeServer WebAnalyzeHttpServer
 
-func NewWebAnalyzeServer() (*WebAnalyzeHttpServer,error) {
+func NewWebAnalyzeServer(appFile string) (*WebAnalyzeHttpServer,error) {
 	was:=new(WebAnalyzeHttpServer)
-	if err:=was.Init("apps.json");err!=nil{
+	if err:=was.init(appFile);err!=nil{
 		return nil,err
 	}
-
 	return was,nil
 }
 
-func (was *WebAnalyzeHttpServer) Init(appsFile string) error {
+func (was *WebAnalyzeHttpServer) init(appsFile string) error {
 	//加载 应用识别定义字典
 	if err:=loadApps(appsFile);err!=nil{
 		return err
@@ -33,6 +40,55 @@ func (was *WebAnalyzeHttpServer) Analyze(url string) (Result) {
 	job:=NewOnlineJob(url, "", nil, 0)
 	result:=dowork(job)
 	return result
+}
+
+func (was *WebAnalyzeHttpServer) HttpHandlerAnalyze(w http.ResponseWriter,r *http.Request)  {
+	if origin := r.Header.Get("Origin"); origin != "" {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST")
+		w.Header().Set("Access-Control-Allow-Headers","*")
+	}
+
+	if r.Method == "OPTIONS" {
+		return
+	}
+	respMsg:=ResponseMsg{}
+	if r.Method!="POST"{
+		respMsg.Code=100
+		respMsg.Msg=fmt.Sprintf("Error:Ivalid Request!")
+		respMsg.Data=""
+	}else{
+		err:=r.ParseForm()
+		if err!=nil{
+			respMsg.Code=100
+			respMsg.Msg=fmt.Sprintf("Error:Ivalid Form Data!")
+			respMsg.Data=""
+		}else{
+			url:=r.PostForm.Get("url")
+			if url!=""{
+				result:=was.Analyze(string(url))
+				jsonValue, err := json.Marshal(result)
+
+				if err!=nil{
+					respMsg.Code=1
+					respMsg.Msg=fmt.Sprintf("Error:%v",err)
+					respMsg.Data=""
+				}else {
+					respMsg.Code=0
+					respMsg.Msg="ok"
+					respMsg.Data=string(jsonValue)
+				}
+			}else{
+				respMsg.Code=100
+				respMsg.Msg=fmt.Sprintf("Error:Ivalid Form Data!")
+				respMsg.Data=""
+			}
+		}
+
+	}
+	w.Header().Set("Content-Type", "application/json")
+	respMsgJson,_:=json.Marshal(respMsg)
+	w.Write(respMsgJson)
 }
 
 func dowork(job *Job) Result {
